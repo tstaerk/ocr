1bf5a30a25ade0867e32d22dbb2481bc9dcaf999
Thorsten Staerk <thorsten@staerk.de>
2024-06-14 23:37:54 +0200

    Update pdf2csv.py

--- pdf2csv.py
+++ pdf2csv.py
@@ -5,7 +5,7 @@ import tabula
 try:
   inputfile=open(sys.argv[1],"rb")
 except:
-  print("This program outputs a table from a PDF file. To start, provide a PDF file as parameter.")
+  print("This program converts a PDF file to CSV. To start, provide a PDF file as parameter.")
 
 from tabula import convert_into
 convert_into(sys.argv[1], sys.argv[1]+".csv", output_format="csv")
